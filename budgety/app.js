79a862c979ed68e2a7c2d14888d15e19512b1518
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2019-07-23 22:35:27 -0300

    add new item to the budget controller

--- budgety/app.js
+++ budgety/app.js
@@ -1,4 +1,6 @@
+/* -------------------------------------------------------------------------------------*/
 // BUDGET CONTROLLER
+/* -------------------------------------------------------------------------------------*/
 var budgetController = (function() {
   
   var Expense = function(id, description, value) {
@@ -23,12 +25,43 @@ var budgetController = (function() {
       inc: 0
     }
   };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      
+
+      // Create new item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push new item into our data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new item
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
+    }
+  };
   
 })();
 
-/* ------------------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------------*/
 // UI CONTROLLER
-/* ------------------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------------*/
 var UIController = (function() {
   
   var DOMstrings = {
@@ -50,9 +83,9 @@ var UIController = (function() {
     }
   };
 })();
-/* ------------------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------------*/
 // GLOBAL APP CONTROLLER
-/* ------------------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------------*/
 var controller = (function(budgetCtrl, UICtrl) {
   
   var setupEventListeners = function() {
@@ -68,11 +101,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
   var ctrlAddItem = function() {
+    var input, newItem;
     // 1. GET THE FIELD INPUT DATA
-    var input = UICtrl.getInput();
-    //console.log(input);
+    input = UICtrl.getInput();
 
     // 2. ADD THE ITEM TO THE BUDGET CONTROLLER 
+    newItem =  budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. ADD THE ITEM TO THE UI
     // 4. CALCULATE THE BUDGET
     // 5. DISPLAY THE BUDGET

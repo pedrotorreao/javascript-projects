b84ab50b5e8b393f3de16a88e699b50c7ec415c0
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2019-07-24 22:29:34 -0300

    clear the inputs after each entry

--- budgety/app.js
+++ budgety/app.js
@@ -104,6 +104,20 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+    clearFields: function() {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -137,8 +151,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 3. ADD THE ITEM TO THE UI
     UICtrl.addListItem(newItem, input.type);
 
-    // 4. CALCULATE THE BUDGET
-    // 5. DISPLAY THE BUDGET
+    // 4. CLEAR THE FIELDS
+    UICtrl.clearFields();
+    // 5. CALCULATE THE BUDGET
+    // 6. DISPLAY THE BUDGET
   };
 
   return {

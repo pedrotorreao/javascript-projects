d5a832006b4ade0fa681bff8966b3e8194fc5cec
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2019-07-26 23:21:57 -0300

    update the budget

--- budgety/app.js
+++ budgety/app.js
@@ -15,6 +15,14 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(cur) {
+      sum += cur.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems: {
       exp: [],
@@ -23,7 +31,9 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
@@ -49,6 +59,32 @@ var budgetController = (function() {
 
       // Return the new item
       return newItem;
+    },
+
+    calculateBudget: function() {
+      // Calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+
+      // Calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+
+      // Calculate the percentage of income that we spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+      
+    },
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
     },
 
     testing: function() {
@@ -141,8 +177,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var updateBudget = function() {
     // 1. CALCULATE THE BUDGET
+    budgetCtrl.calculateBudget();
+
     // 2. RETURN THE BUDGET
+    var budget = budgetCtrl.getBudget();
+
     // 3. DISPLAY THE BUDGET ON THE UI
+    console.log(budget);
   }
 
 

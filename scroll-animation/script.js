02813387b104968e78b9ae57728101e0a14a49f9
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-02-20 22:54:05 -0300

    update layout and add comments to js file

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -1,5 +1,14 @@
 const boxes = document.querySelectorAll('.box');
 
+/* -- addEventListener():
+    sets up a function that will be called whenever the specified event is delivered to the target. 
+    Common targets are Element, Document, and Window, but the target may be any object that supports 
+    events (such as XMLHttpRequest). addEventListener() works by adding a function or an object 
+    that implements EventListener to the list of event listeners for the specified event type on 
+    the EventTarget on which it's called.
+    Syntax:
+        target.addEventListener(type, listener [, options]);
+    */
 window.addEventListener('scroll', checkBoxes);
 
 checkBoxes();
@@ -8,6 +17,10 @@ function checkBoxes() {
     const triggerBottom = (window.innerHeight / 5) * 4;
 
     boxes.forEach(box => {
+        /* -- getBoundingClientRect():
+        returns a DOMRect object providing information about the size of an element 
+        (in this case, the boxes) and its position relative to the viewport.
+        */
         const boxTop = box.getBoundingClientRect().top;
 
         if(boxTop < triggerBottom) {
